40ef49c8c28b73f743d9fe8ae0f5c0fa1f0a1408
suatoruc <suattoruc@gmail.com>
2022-11-19 22:18:21 +0300

    end

--- src/test/java/NewFeatures.java
+++ src/test/java/NewFeatures.java
@@ -1,10 +1,17 @@
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import java.io.*;
+import java.time.Duration;
+import java.util.List;
 
 public class NewFeatures {
     static  WebDriver driver;
@@ -12,24 +19,28 @@ public class NewFeatures {
    @Test
    public void chromeTest(){
    driver=new ChromeDriver();
-       driver.get("https://www.google.com");
-   }
+   driver.manage().window().maximize();
+   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+       driver.get("www.google.com");
+        }
 
     @Test
     public void edgeTest(){
         driver=new EdgeDriver();
         driver.get("https://www.google.com");
+
     }
 
     @Test
     public void firefoxTest(){
         driver=new FirefoxDriver();
         driver.get("https://www.google.com");
+
     }
 
     @AfterMethod
     public void teardown(){
-       driver.quit();
+      // driver.quit();
 
     }
 
